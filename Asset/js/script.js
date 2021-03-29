645b053a011ccfd488ec1fcffc1def42fac7f362
Vicheka Phat <vickphat@gmail.com>
2021-03-28 23:04:16 -0400

    change time blocks ids, added save button function

--- Asset/js/script.js
+++ Asset/js/script.js
@@ -1,9 +1,19 @@
-var currentDay = document.querySelector("#currentDay")
-
 // Displays current date
-function displayTime() {
-    var time = moment().format("dddd MMMM Do YYYY");    
-    currentDay.append(time);
-  }
+var time = moment().format("dddd MMMM Do YYYY");
+$("#currentDay").append(time);
+console.log(time)
 
-  displayTime();
+// saveBtn click listener 
+$(".saveBtn").click(function () {
+
+// Get nearby values of the description in JQuery
+var text = $(this).siblings(".description").val();
+var timeBlock = $(this).parent().attr("id");
+console.log(this, text, timeBlock)
+
+// Saves text input and time block into local storage
+localStorage.setItem(timeBlock, text);
+
+})
+
+
